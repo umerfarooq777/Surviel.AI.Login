57b3518193c88cb82f3881478ffc9f42ed8f45e3
Umer Farooq <98796369+umerfarooq777@users.noreply.github.com>
2022-06-02 04:53:08 -0700

    animation done

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -5,8 +5,11 @@ import Spline from '@splinetool/react-spline';
 function App() {
   return (
     <div className="App">
-      <Spline scene="https://prod.spline.design/Uw8mZUAybxHp3dVb/scene.splinecode" />
+      <Spline scene="https://prod.spline.design/DyQVJiBSB7JeSqlC/scene.splinecode" />
+   
+      <Spline scene="https://prod.spline.design/Nbh3bhOhWse1LlzK/scene.splinecode" />
     </div>
+  
   );
 }
 
